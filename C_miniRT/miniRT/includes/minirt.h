c90d6c351fc2935ba872a7eaa2adf252a1317d5e
Takumi Nagoshi <tnagoshi@c1r3s1.42tokyo.jp>
2021-03-24 18:46:40 +0900

    prepare minirt.h minirt.c

---
+++ C_miniRT/miniRT/includes/minirt.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnagoshi <tnagoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/24 18:19:54 by tnagoshi          #+#    #+#             */
+/*   Updated: 2021/03/24 18:20:34 by tnagoshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+
+/*
+** minilbx
+*/
+
+# include "mlx.h"
+
+#endif
